41bf379df5853bb8e5f8a579b3ecff7d857c19ff
Debjit Pal <debjitpal5040@gmail.com>
2022-07-28 21:24:40 +0530

    ...

---
+++ Machine Learning/Supervised Learning/Regression/Decision Tree Regression/Heuristic_Decision_Tree_Regression.py
@@ -0,0 +1,137 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+data = pd.read_csv("Machine Learning/Supervised Learning/Regression/Decision Tree Regression/Dataset/IceCreamData.csv")
+class Node():
+    def __init__(self, feature_index=None, threshold=None, left=None, right=None, var_red=None, value=None):
+        ''' constructor ''' 
+        # for decision node
+        self.feature_index = feature_index
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.var_red = var_red
+        # for leaf node
+        self.value = value
+
+class DecisionTreeRegressor():
+    def __init__(self, min_samples_split=2, max_depth=3):
+        ''' constructor '''
+        # initialize the root of the tree 
+        self.root = None
+        # stopping conditions
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        
+    def build_tree(self, dataset, curr_depth=0):
+        ''' recursive function to build the tree '''
+        X, Y = dataset[:,:-1], dataset[:,-1]
+        num_samples, num_features = np.shape(X)
+        best_split = {}
+        # split until stopping conditions are met
+        if num_samples>=self.min_samples_split and curr_depth<=self.max_depth:
+            # find the best split
+            best_split = self.get_best_split(dataset, num_samples, num_features)
+            # check if information gain is positive
+            if best_split["var_red"]>0:
+                # recur left
+                left_subtree = self.build_tree(best_split["dataset_left"], curr_depth+1)
+                # recur right
+                right_subtree = self.build_tree(best_split["dataset_right"], curr_depth+1)
+                # return decision node
+                return Node(best_split["feature_index"], best_split["threshold"], left_subtree, right_subtree, best_split["var_red"])
+        # compute leaf node
+        leaf_value = self.calculate_leaf_value(Y)
+        # return leaf node
+        return Node(value=leaf_value)
+    
+    def get_best_split(self, dataset, num_samples, num_features):
+        ''' function to find the best split '''
+        # dictionary to store the best split
+        best_split = {}
+        max_var_red = -float("inf")
+        # loop over all the features
+        for feature_index in range(num_features):
+            feature_values = dataset[:, feature_index]
+            possible_thresholds = np.unique(feature_values)
+            # loop over all the feature values present in the data
+            for threshold in possible_thresholds:
+                # get current split
+                dataset_left, dataset_right = self.split(dataset, feature_index, threshold)
+                # check if childs are not null
+                if len(dataset_left)>0 and len(dataset_right)>0:
+                    y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
+                    # compute information gain
+                    curr_var_red = self.variance_reduction(y, left_y, right_y)
+                    # update the best split if needed
+                    if curr_var_red>max_var_red:
+                        best_split["feature_index"] = feature_index
+                        best_split["threshold"] = threshold
+                        best_split["dataset_left"] = dataset_left
+                        best_split["dataset_right"] = dataset_right
+                        best_split["var_red"] = curr_var_red
+                        max_var_red = curr_var_red
+        # return best split
+        return best_split
+    
+    def split(self, dataset, feature_index, threshold):
+        ''' function to split the data '''
+        dataset_left = np.array([row for row in dataset if row[feature_index]<=threshold])
+        dataset_right = np.array([row for row in dataset if row[feature_index]>threshold])
+        return dataset_left, dataset_right
+
+    def variance_reduction(self, parent, l_child, r_child):
+        ''' function to compute variance reduction '''
+        weight_l = len(l_child) / len(parent)
+        weight_r = len(r_child) / len(parent)
+        reduction = np.var(parent) - (weight_l * np.var(l_child) + weight_r * np.var(r_child))
+        return reduction
+    
+    def calculate_leaf_value(self, Y):
+        ''' function to compute leaf node '''
+        val = np.mean(Y)
+        return val
+
+    def print_tree(self, tree=None, indent=" "):
+        ''' function to print the tree '''
+        if not tree:
+            tree = self.root
+        if tree.value is not None:
+            print(tree.value)
+        else:
+            print("X_"+str(tree.feature_index), "<=", tree.threshold, "?", tree.var_red)
+            print("%sleft:" % (indent), end="")
+            self.print_tree(tree.left, indent + indent)
+            print("%sright:" % (indent), end="")
+            self.print_tree(tree.right, indent + indent)
+    
+    def fit(self, X, Y):
+        ''' function to train the tree '''
+        dataset = np.concatenate((X, Y), axis=1)
+        self.root = self.build_tree(dataset)
+        
+    def make_prediction(self, x, tree):
+        ''' function to predict new dataset '''
+        if tree.value!=None: 
+            return tree.value
+        feature_val = x[tree.feature_index]
+        if feature_val<=tree.threshold:
+            return self.make_prediction(x, tree.left)
+        else:
+            return self.make_prediction(x, tree.right)
+    
+    def predict(self, X):
+        ''' function to predict a single data point '''
+        preditions = [self.make_prediction(x, self.root) for x in X]
+        return preditions
+
+X = data.iloc[:, :-1].values
+Y = data.iloc[:, -1].values.reshape(-1,1)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=0)
+regressor = DecisionTreeRegressor()
+regressor.fit(X_train,Y_train)
+regressor.print_tree()
+Y_pred = regressor.predict(X_test) 
+print((mean_squared_error(Y_test, Y_pred, squared=False)))
