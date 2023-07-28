8287c27107d688b7e6a604433e3daba22f9a6e6e
Debjit Pal <debjitpal5040@gmail.com>
2023-07-28 13:23:28 +0530

    hyper
    slr
    
    polynomial
    
    classification done
    
    dtc
    
    puhs
    
    ....
    
    Update Simple_Linear_Regression.ipynb
    
    poly
    
    models

--- Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Multiple Linear Regression/Heuristic_Multiple_Linear_Regression.py
+++ Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Multiple Linear Regression/Heuristic_Multiple_Linear_Regression.py
@@ -1,18 +1,19 @@
+"""Heuristic Multiple Linear Regression"""
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.markers import MarkerStyle
 
 # read the csv file
-df = pd.read_csv('Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Multiple Linear Regression/Startups.csv')
-x = df['Research'].tolist()  # x axis values
-y = df['Marketing'].tolist()  # y axis values
-z = df['Profit'].tolist()  # z axis values
-x2 = [x[i]**2 for i in range(len(x))]  # squaring x values
-y2 = [y[i]**2 for i in range(len(y))]  # squaring y values
-xy = [x[i]*y[i] for i in range(len(x))]  # multiplying x and y values
-xz = [x[i]*z[i] for i in range(len(x))]  # multiplying x and z values
-yz = [y[i]*z[i] for i in range(len(y))]  # multiplying y and z values
+df = pd.read_csv("Datasets/Startups.csv")
+x = df["Research"].tolist()  # x axis values
+y = df["Marketing"].tolist()  # y axis values
+z = df["Profit"].tolist()  # z axis values
+x2 = [x[i] ** 2 for i in range(len(x))]  # squaring x values
+y2 = [y[i] ** 2 for i in range(len(y))]  # squaring y values
+xy = [x[i] * y[i] for i in range(len(x))]  # multiplying x and y values
+xz = [x[i] * z[i] for i in range(len(x))]  # multiplying x and z values
+yz = [y[i] * z[i] for i in range(len(y))]  # multiplying y and z values
 X = sum(x)  # sum of x values
 Y = sum(y)  # sum of y values
 Z = sum(z)  # sum of z values
@@ -30,20 +31,24 @@ a = ans[0]  # a value
 b = ans[1]  # b value
 c = ans[2]  # c value
 fig = plt.figure(figsize=(10, 10), dpi=150)
-plt.style.use('dark_background')  # changing the background color
-ax = fig.add_subplot(111, projection='3d')  # syntax for 3-D projection
-ax.scatter(x, y, z, marker=MarkerStyle('.'), color='red')  # plotting points
+plt.style.use("dark_background")  # changing the background color
+ax = fig.add_subplot(111, projection="3d")  # syntax for 3-D projection
+ax.scatter(x, y, z, marker=MarkerStyle("."), color="red")  # plotting points
 ax.set_xticks(np.arange(15000, 200000, 25000))
 ax.set_yticks(np.arange(25000, 475000, 50000))
 ax.set_zticks(np.arange(60000, 220000, 20000))
-ax.set_xlabel('Research')
-ax.set_ylabel('Marketing')
-ax.set_zlabel('Profit')
-plt.title('3D Scatter Plot with best fit plane')
+ax.set_xlabel("Research")
+ax.set_ylabel("Marketing")
+ax.set_zlabel("Profit")
+plt.title("3D Scatter Plot with best fit plane")
 p = np.linspace(15000, 200000, 100)  # creating a list of 100 values
 q = np.linspace(25000, 475000, 100)  # creating a list of 100 values
 p, q = np.meshgrid(p, q)  # creating a meshgrid
-eq = a+b*p+c*q  # equation of plane
-ax.plot_surface(p, q, eq, alpha=0.3, color='blue')  # plotting plane
-plt.savefig('Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Multiple Linear Regression/Images/Heuristic_Multiple_Linear_Regression.png', bbox_inches='tight', dpi=150)  # saving the plot
+eq = a + b * p + c * q  # equation of plane
+ax.plot_surface(p, q, eq, alpha=0.3, color="blue")  # plotting plane
+plt.savefig(
+    "Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Multiple Linear Regression/Images/Heuristic_Multiple_Linear_Regression.png",
+    bbox_inches="tight",
+    dpi=150,
+)  # saving the plot
 plt.show()  # showing the plot
--- Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Simple Linear Regression/Heuristic_Simple_Linear_Regression.py
+++ Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Simple Linear Regression/Heuristic_Simple_Linear_Regression.py
@@ -1,14 +1,15 @@
+"""Heuristic Simple Linear Regression"""
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.markers import MarkerStyle
 
 # read the csv file
-df = pd.read_csv('Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Simple Linear Regression/Salary_Data.csv')
-x = df['YearsExperience'].tolist()  # x axis values
-y = df['Salary'].tolist()  # y axis values
-xy = [x[i]*y[i] for i in range(len(x))]  # multiplying x and y values
-x2 = [x[i]**2 for i in range(len(x))]  # squaring x values
+df = pd.read_csv("Datasets/SalaryData.csv")
+x = df["YearsExperience"].tolist()  # x axis values
+y = df["Salary"].tolist()  # y axis values
+xy = [x[i] * y[i] for i in range(len(x))]  # multiplying x and y values
+x2 = [x[i] ** 2 for i in range(len(x))]  # squaring x values
 X = sum(x)  # sum of x values
 Y = sum(y)  # sum of y values
 X2 = sum(x2)  # sum of x2 values
@@ -22,15 +23,19 @@ A_inv = np.linalg.inv(A)  # inverse of A
 ans = np.matmul(A_inv, B)  # multiplying A inverse and B
 a = ans[0]  # a value
 b = ans[1]  # b value
-plt.style.use('dark_background')  # changing the background color
-plt.scatter(x, y, marker=MarkerStyle('.'), color='red')  # plotting points
+plt.style.use("dark_background")  # changing the background color
+plt.scatter(x, y, marker=MarkerStyle("."), color="red")  # plotting points
 plt.xticks(np.arange(1, 12, 1))  # setting x axis values
 plt.yticks(np.arange(37000, 128000, 10000))  # setting y axis values
-plt.xlabel('Years of Experience')  # setting x label
-plt.ylabel('Salary')  # setting y label
-plt.title('2D Scatter plot with best fit line')  # setting title
+plt.xlabel("Years of Experience")  # setting x label
+plt.ylabel("Salary")  # setting y label
+plt.title("2D Scatter plot with best fit line")  # setting title
 p = np.linspace(0, 11, 100)  # creating a list of 100 values
-q = b*p+a  # equation of line
-plt.plot(p, q, color='blue')  # plotting line
-plt.savefig('Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Simple Linear Regression/Images/Heuristic_Simple_Linear_Regression.png', bbox_inches='tight', dpi=150)  # saving the plot
+q = b * p + a  # equation of line
+plt.plot(p, q, color="blue")  # plotting line
+plt.savefig(
+    "Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Multiple Linear Regression/Images/Heuristic_Multiple_Linear_Regression.png",
+    bbox_inches="tight",
+    dpi=150,
+)  # saving the plot
 plt.show()  # showing the graph
