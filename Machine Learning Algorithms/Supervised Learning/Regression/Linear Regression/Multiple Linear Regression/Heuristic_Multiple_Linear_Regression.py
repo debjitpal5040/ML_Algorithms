d0b64309b3d9be572bb7b35ea46a52cd6de97b5d
Debjit Pal <debjitpal5040@gmail.com>
2024-01-26 19:23:46 +0530

    upgrade more
    upgrade
    
    more
    
    tune jo na kaha
    
    why
    
    Dt
    
    out

--- Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Multiple Linear Regression/Heuristic_Multiple_Linear_Regression.py
+++ Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Multiple Linear Regression/Heuristic_Multiple_Linear_Regression.py
@@ -25,7 +25,7 @@ Y2 = sum(y2)  # sum of y2 values
 n = len(x)  # number of values
 A = [[n, X, Y], [X, X2, XY], [Y, XY, Y2]]  # matrix A
 B = [Z, XZ, YZ]  # matrix B
-A_inv = np.linalg.inv(A)  # inverse of A
+A_inv = np.linalg.inv(A)  # inverse of A # type: ignore
 ans = np.matmul(A_inv, B)  # multiplying A inverse and B
 a = ans[0]  # a value
 b = ans[1]  # b value
@@ -36,16 +36,16 @@ ax = fig.add_subplot(111, projection="3d")  # syntax for 3-D projection
 ax.scatter(x, y, z, marker=MarkerStyle("."), color="red")  # plotting points
 ax.set_xticks(np.arange(15000, 200000, 25000))
 ax.set_yticks(np.arange(25000, 475000, 50000))
-ax.set_zticks(np.arange(60000, 220000, 20000))
+ax.set_zticks(np.arange(60000, 220000, 20000)) # type: ignore
 ax.set_xlabel("Research")
 ax.set_ylabel("Marketing")
-ax.set_zlabel("Profit")
+ax.set_zlabel("Profit") # type: ignore
 plt.title("3D Scatter Plot with best fit plane")
 p = np.linspace(15000, 200000, 100)  # creating a list of 100 values
 q = np.linspace(25000, 475000, 100)  # creating a list of 100 values
 p, q = np.meshgrid(p, q)  # creating a meshgrid
 eq = a + b * p + c * q  # equation of plane
-ax.plot_surface(p, q, eq, alpha=0.3, color="blue")  # plotting plane
+ax.plot_surface(p, q, eq, alpha=0.3, color="blue")  # plotting plane # type: ignore
 plt.savefig(
     "Machine Learning Algorithms/Supervised Learning/Regression/Linear Regression/Multiple Linear Regression/Images/Heuristic_Multiple_Linear_Regression.png",
     bbox_inches="tight",
